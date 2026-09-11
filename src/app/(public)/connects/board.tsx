d604f1bb86be70bf1f26034facaa0a232a7afc83
Minjae Kim <jadenkim1234567@gmail.com>
2026-09-11 16:43:26 +0900

    Update board.tsx

--- src/app/(public)/connects/board.tsx
+++ src/app/(public)/connects/board.tsx
@@ -91,7 +91,7 @@ export function Board({
             <Link href="/mbti" className="mbti-card">
               <p className="mbti-eyebrow">CONNECT MBTI</p>
               <p className="mbti-title">
-                
+                내 성향에 맞는
                 <br />
                 커넥트 찾기
               </p>
